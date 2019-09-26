28af7ffa1306ada778a4b4839aa551dccc4fa241
Timur Sepety <sepety@shaolin.ru>
2019-09-26 14:53:13 +0300

    corect work of mc

--- maneger1.py
+++ maneger1.py
@@ -16,48 +16,44 @@ def open_lib_and_creat_dict(myfile):
         delta = then - now
         print(delta.microseconds)
 
+def get_shop_list_by_dishes(menu, dishes, person_count):
+    ee = {}
+    for name in dishes:
+        for ingridient in menu[name.strip()]:
+            # ingridient = dict[name]
+            # print(ingridient)
+            if ingridient['ingridient_name'] in ee.keys():
+                ee[ingridient['ingridient_name']]['quantity'] += ingridient['quantity'] * person_count
+            else:
+                ee[ingridient['ingridient_name']] = {
+                    'measure': ingridient['measure'],
+                    'quantity': ingridient['quantity'] * person_count
+                }
+    return ee
 
-with open_lib_and_creat_dict('cookbook') as read_you_cbook:
-    def get_shop_list_by_dishes(menu, dishes, person_count):
-        ee = {}
-        for name in dishes:
-            for ingridient in menu[name.strip()]:
-                # ingridient = dict[name]
-                # print(ingridient)
-                if ingridient['ingridient_name'] in ee.keys():
-                    ee[ingridient['ingridient_name']]['quantity'] += ingridient['quantity'] * person_count
-                else:
-                    ee[ingridient['ingridient_name']] = {
-                        'measure': ingridient['measure'],
-                        'quantity': ingridient['quantity'] * person_count
-                    }
+def read_you_cbook(name_of_file):
+    menu = {}
+    with open_lib_and_creat_dict(name_of_file) as f:
+        while True:
+            title = f.readline().strip()
+            # print(type(line))
+            if not title:
+                break
+            menu[title] = []
+            num = f.readline()
+            n = int(num)
 
-        return ee
+            for i in range(n):
+                [ingridient_name, quantity, measure] = f.readline().split('|')
+                menu[title].append({
+                    'ingridient_name': ingridient_name.strip(),
+                    'quantity': int(quantity.strip()),
+                    'measure': measure.strip()
+                })
 
+            f.readline()
 
-    def read_you_cbook(name_of_file):
-        menu = {}
-        with open(name_of_file) as f:
-            while True:
-                title = f.readline().strip()
-                # print(type(line))
-                if not title:
-                    break
-                menu[title] = []
-                num = f.readline()
-                n = int(num)
-
-                for i in range(n):
-                    [ingridient_name, quantity, measure] = f.readline().split('|')
-                    menu[title].append({
-                        'ingridient_name': ingridient_name.strip(),
-                        'quantity': int(quantity.strip()),
-                        'measure': measure.strip()
-                    })
-
-                f.readline()
-
-        return menu
+    return menu
 
 
 def main():
@@ -69,7 +65,10 @@ def main():
 
     shop = get_shop_list_by_dishes(menu, names, number)
     print(shop)
-
+    print(datetime.datetime.now())
+    then = datetime.datetime.now()
+    delta = then - now
+    print(delta.microseconds)
 
 if __name__ == '__main__':
     main()
